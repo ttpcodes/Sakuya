e09d2693c1b738b96a78d540ac432c10afe20e49
Michael William Le Nguyen <michael@mail.ttp.codes>
2020-09-06 20:05:10 -0400

    main: log command stack traces

--- sakuya/__main__.py
+++ sakuya/__main__.py
@@ -2,12 +2,17 @@ from discord.ext.commands import Bot
 
 from sakuya import Config, generate_embed_template
 
+from logging import getLogger
+
+logger = getLogger(__name__)
+
 
 class Sakuya(Bot):
     def __init__(self):
         super().__init__(command_prefix=Config['discord']['prefix'])
 
     async def on_command_error(self, ctx, exception):
+        logger.warning('error running command {}:'.format(ctx.message.content), exc_info=exception)
         embed = generate_embed_template(ctx, 'Error Running Command', True)
         embed.description = str(exception)
         await ctx.send(embed=embed)
